bbbad8978bf5af290ce36315941ed795629d52a0
Rodrigo Medina <rodri_0406@hotmail.com>
2022-02-19 17:45:34 -0300

    se crea repo

---
+++ src/main/java/com/rebel/alliance/app/RebelAllianceApplication.java
@@ -0,0 +1,13 @@
+package com.rebel.alliance.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebelAllianceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebelAllianceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebel/alliance/app/RebelAllianceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebel.alliance.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebelAllianceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
